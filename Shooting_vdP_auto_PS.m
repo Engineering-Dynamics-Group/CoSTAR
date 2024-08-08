3f71521ec9c613b00991ed65ac65a32935b95778
IFM-PC158\Admin <alexander.seifert@uni-kassel.de>
2024-08-08 10:51:39 +0200

    Main function for autonomous case added.

---
+++ Shooting_vdP_auto_PS.m
@@ -0,0 +1,33 @@
+clear all; close all; clc;
+addpath(genpath(cd));
+
+%% DUFFING EXAMPLE
+%% Parameters and Equation
+mu_limit = [0.2, 2.5];
+epsilon = mu_limit(1);
+
+param = {epsilon};
+Fcn = @(t,z,param)vdP_auto_ap(t,z,param);
+
+active_parameter = 1;
+% non_auto_freq = @(mu) mu;
+auto_freq = 1;
+
+%% Define the input options
+options.system              = costaropts('order',1,'rhs',Fcn,'param',param,'dim',2);    %Properties of the System
+options.opt_sol             = costaropts('stability','off','cont','on','auto_freq',auto_freq,'sol_type','periodic','approx_method','MSHM','act_param',active_parameter);   %Properties of the solution
+options.opt_cont            = costaropts('step_control','angle','mu_limit',mu_limit,'step_width',0.1,'step_width_limit',[0.001,0.3]);        %Properties for continuation
+options.opt_approx_method   = costaropts('solver','ode45','n_shoot',5);
+options.opt_init            = costaropts('ic',[0;1]);
+options.opt_stability       = costaropts('iterate_bfp','off');
+
+%% Continuation
+[S,DYN] = costar(options);
+
+opts = costaropts('zaxis','max2');
+S.contplot(DYN,opts);
+
+
+
+figure('Color','white')
+plot(S.arclength,abs(S.multipliers))
