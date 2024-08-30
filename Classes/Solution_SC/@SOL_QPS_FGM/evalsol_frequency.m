24f3ba2a459f75b39137c72e15804a9099571918
Julian Vogelei <julian.vogelei@uni-kassel.de>
2024-08-30 16:58:18 +0200

    Adaptions of postprocessing methods: Part 4 (phase angle in frequency solution space is now completely added as additional output)

--- Classes/Solution_SC/@SOL_QPS_FGM/evalsol_frequency.m
+++ Classes/Solution_SC/@SOL_QPS_FGM/evalsol_frequency.m
@@ -1,5 +1,5 @@
 % Method of SOL_QPS_FGM: This method does evaluate the Fourier spectrum directly
-% The method gives back the absolute value of the complex fourier transform 
+% The method returns the absolute value and the phase angle of the complex fourier transform 
 %
 % @obj:     Solution subclass object
 % @DYN:     DynamicalSystem object
@@ -11,64 +11,74 @@
 % @f:       Array of the frequency points: This must(!) be a [options.resolution/2 x 1 x n_evals]  dimensional array !!!
 % n_evals:  Number of curve points to be evaluated 
 
-function [P,mu,f] = evalsol_frequency(obj,DYN,options)
+function [P,a,mu,f] = evalsol_frequency(obj,DYN,options)
 
     index   =   options.index;
-    N       =   numel(index);                       %Number of solutions asked for  
-    dim     = DYN.dim;                          %dimension of the state space
+    N       =   numel(index);       % Number of solutions asked for
+    dim     = DYN.dim;              % Dimension of the state space
+    res     = options.resolution;   % Resolution 
     counter = 0;
 
-    %%% THESE LINES MUST BE CHANGED IF HMATRIX IS ADAPTED IN FUTURE RELEASE
     %Initialise:
-    P = zeros(options.resolution,dim,N);
-    a = zeros(options.resolution,dim,N);
-    f = zeros(options.resolution,1,N);
+    P = zeros(res/2,dim,N);
+    a = zeros(res/2,dim,N);
+    f = zeros(res/2,1,N);
     
-
-    %Task: define a frequency vector with the demanded resolution, which
-    %includes all known frequencies freq.*hmatrix! In other words: the
-    %frequencies apart from freq.*hamtrix are filler frequencies with an
-    %amplitude of 0.
+    % Task: define a frequency vector with the demanded resolution, which includes all known frequencies freq.*hmatrix! 
+    % In other words: the frequencies apart from freq.*hamtrix are filler frequencies with an amplitude of 0.
     
-    %TODO: Vectorise this code for better performance.
+    % TODO: Vectorise this code for better performance.
 
     for k = index
 
-        counter = counter +1;
-
-        s       =   obj.s{1,k};
-        n_hh = (size(s,1)/dim-1)/2+1;           %Compute the number of higher harmonics
-        hmatrixtmp = obj.hmatrix{1,k};
+        counter = counter + 1;
+        s = obj.s{1,k};
+        n_hh = (size(s,1)/dim-1)/2+1;               % Compute the number of higher harmonics
   
 
-        %This could also be done in the real domain... however: this is how we
-        %do it in the residuum equation
-
-        FC          = [s(1:dim,:);s((dim+1):(n_hh)*dim,:)-1i.*s(((n_hh)*dim+1):end,:)];        %Assemble complex Fourrier vector    
+        % This could also be done in the real domain... however: this is how we do it in the residuum equation
+        FC          = [s(1:dim,:);s((dim+1):(n_hh)*dim,:)-1i.*s(((n_hh)*dim+1):end,:)];      % Assemble complex Fourrier vector    
         FC          = reshape(FC,dim,n_hh);
         freq        = obj.freq(:,k);
+        hmatrix     = obj.hmatrix{1,k};             % Harmonics
 
-        [frq_val,idx_tmp] = sort(abs(freq.'*hmatrixtmp));   %Some frequency combination might be negative. frq_val thus must be sorted in ascending order
-        FCtmp = abs(FC(:,idx_tmp));                       %Order the fourier-coefficients accordingly to the frequencies.
+        [frq_sort,idx_sort] = sort(abs(freq.'*hmatrix));    % Some frequency combination might be negative. frq_sort thus must be sorted in ascending order
+        A = abs(FC(:,idx_sort));                            % Order the fourier-coefficients accordingly to the frequencies and compute the absolute amplitudes
+        alpha = atan2(-imag(FC),real(FC));                  % Order the fourier-coefficients accordingly to the frequencies and compute the phase angles
 
-        if options.resolution > n_hh
+        if res > n_hh
 
+            f_val = sort([frq_sort(2:end),linspace(0,1.1*max(frq_sort),res/2-numel(frq_sort(2:end)))]);   % Add as many zeros as are needed for the the vector to have the length resolution  
+            f_val_u = unique(f_val);                        % Remove the doubled frequencies (in most cases, there should be none since it is very unlikely that linspace hits a value of frq_sort(2:end-1))
+            % Most of the values of f_val_u are equidistantly distributed within [0,max(frq_sort)]. However, the frequencies ...
+            % frq_sort are placed somewhere in between, which can lead to ugly plots. That is why the frequency values between ...
+            % the frq_values (overall numel(frq_values)-1 sections) are distributed equidistantly within each section again.
+            [~,idx] = intersect(f_val_u,[frq_sort,1.1*max(frq_sort)]);          % Find the index positions where the frequencies frq_sort are located
+            for i = 1:(numel(idx)-1)
+                f_val_u(idx(i):idx(i+1)) = linspace(f_val_u(idx(i)),f_val_u(idx(i+1)),numel(idx(i):idx(i+1)));
+            end
+            % Now that the frequency values are distributed equidistantly as far as possible, we need to add some frequencies in case that numel(f_val_u) ~= res/2
+            spacing = f_val_u(end) - f_val_u(end-1);        % Compute a mean spacing for adding frequencies
+            % spacing = mean(diff(f_val));                  % OLD CODE: spacing
+            f(:,1,counter) = [f_val_u, max(f_val_u) + (1:(numel(f_val)-numel(f_val_u)))*spacing].';     % Add the number of removed frequencies at the end with the appropriate spacing to restore the correct length
 
-            f_val = sort([frq_val,linspace(0,max(frq_val),options.resolution-numel(frq_val))]); %Add as many zeros as are needed for the the vector to have the length resolution
-            spacing = mean(diff(f_val));    %Compute a mean spacing
-            f_val_u = unique(f_val);        %Get out the doubled frequencies
-            f(:,1,counter) = [f_val_u, max(f_val_u) + (1:(numel(f_val)-numel(f_val_u)))*spacing].'; %Simply adding the needed number of frequencies at the end with the appropriate spacing in between
-
-            %Now add the FCtmp values at the apropriate positions:
-            [~,idx] = intersect(f(:,1,counter),frq_val);          %Find the indices of the contained frequencies
-            P(idx,:,counter) = FCtmp.';                           %Write the value
+            % Now add the amplitude and angle values at the appropiate positions. 
+            % We can use idx(1:end-1) for that (f(idx(end)) = 1.1*max(frq_sort))
+            % [~,idx] = intersect(f(:,1,counter),frq_sort); % OLD CODE: Find the indices of the contained frequencies    
+            P(idx(1:end-1),:,counter) = A.';                % Write the value
+            a(idx(1:end-1),:,counter) = alpha.';            % Write the value
+            
         else
-            f(:,1,counter) = freq.*hmatrixtmp(1:options.resolution);
-            P(:,:,kcounter) = FCtmp(:,1:options.resolution).';
-            warning(append('Request of frequency for Fourier-Galerkin method: Your requested options.resolution = ',num2str(options.resolution),' is smaller than the number of frequencies = ', num2str(numel(hmatrixtmp)),'. Consider raising the resolution.'));
+
+            f(:,1,counter) = freq.*hmatrix(1:res);
+            P(:,:,counter) = A(:,1:res).';
+            warning(append('Request of frequency for Fourier-Galerkin method: Your requested options.resolution = ',num2str(res),...
+                           ' is smaller than the number of frequencies = ', num2str(numel(hmatrix)),'. Consider raising the resolution.'));
+        
         end
+
     end
 
-    mu(1,:)  = obj.mu(1,options.index);  
+    mu(1,:)  = obj.mu(1,index);  
 
 end
--- test/test_FGM_QPS_duffing.m
+++ test/test_FGM_QPS_duffing.m
@@ -21,7 +21,14 @@ Fcn = @(t,z,param)duffing_ap_qp(t,z,param);
 non_auto_freq = @(mu) [mu,ratio.*mu];
 
 
-%% Init data
+% Init data
+K3 = [0, 1, 0; 0, 0, 1];
+
+c_max2 = [  0,     0;                      % Fourier coefficients for mu = 2
+          -0.2, -0.07];
+s_max2 = [-0.1, -0.025;
+            0,     0  ];
+
 % c_max =     [ 0.1188,    0.1443;
 %              -0.0104,   -0.0075];
 % s_max =     [-0.1049,   -0.0539;
@@ -33,23 +40,17 @@ non_auto_freq = @(mu) [mu,ratio.*mu];
 % c_max2 = [c_max, 0.1.*ones(2,14)];
 % s_max2 = [s_max, 0.1.*ones(2,14)];
 
-K3 = [0, 1, 0; 0, 0, 1];
-
-c_max2 = [  0,     0;                      % Fourier coefficients for mu = 2
-          -0.2, -0.07];
-s_max2 = [-0.1, -0.025;
-            0,     0  ];
-
 
 %% Properties
-options.system   = costaropts('order',1,'rhs',Fcn,'param',param,'dim',2);                               %Properties of the System
-options.opt_sol  = costaropts('stability','off','cont','on','non_auto_freq',non_auto_freq,'sol_type','quasiperiodic','approx_method','fourier-galerkin','act_param',active_parameter);  %Properties of the solution
+options.system   = costaropts('order',1,'rhs',Fcn,'param',param,'dim',2);                                                   % Properties of the System
+options.opt_sol  = costaropts('stability','off','cont','on','non_auto_freq',non_auto_freq,'sol_type','quasiperiodic',...
+                              'approx_method','fourier-galerkin','act_param',active_parameter);                             % Properties of the solution
 options.opt_init = costaropts('C0',zeros(2,1),'Cmatrix',c_max2,'Smatrix',s_max2,'Hmatrix',K3);
-options.opt_approx_method = costaropts('n_FFT',2^6,'error_limit',[1e-3 1e-2]);                          %Properties for approx_method
-options.opt_cont = costaropts('step_control','angle','pred','parable','mu_limit',mu_limit,'direction',-1,'display','step_control_info');   %Properties for continuation
+options.opt_approx_method = costaropts('n_FFT',2^6,'error_limit',[1e-3 1e-2]);                                              % Properties for approx_method
+options.opt_cont = costaropts('pred','parable','mu_limit',mu_limit,'direction',-1,'display','step_control_info');           % Properties for continuation
 
 tic
-[S,DYN] = costar(options);                                                                                                       %Calculate initial solution and continue the curve to set limits
+[S,DYN] = costar(options);
 toc
 
 
