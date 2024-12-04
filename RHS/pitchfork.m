09ff2bb19e7f0f2c8b2603b409abf420776e0fe8
Alexander Seifert <alexander.seifert@uni-kassel.de>
2024-12-04 09:53:20 +0100

    Old rhs-files removed

--- RHS/pitchfork.m
+++
@@ -1,5 +0,0 @@
-function f = pitchfork(z,mu,gamma)
-
-f(1,1) = mu.*z(1)-z(1).^3+gamma;
-
-end
--- RHS/vdP_auto.m
+++
@@ -1,6 +0,0 @@
-function f = vdP_auto(t,z,epsilon)
-
-f(1,1) = z(2,1);
-f(2,1) = -z(1,1)-epsilon.*(z(1,1).^2-1).*z(2,1);
-
-end
