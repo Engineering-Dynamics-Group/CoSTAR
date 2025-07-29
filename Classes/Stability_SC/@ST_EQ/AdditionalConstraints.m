f7b4ad124d96e985fdecea4a2e430e87db106517
Alexander Seifert <alexander.seifert@uni-kassel.de>
2025-07-29 16:17:33 +0200

    Method 'AdditionalConstraints' written. Determines whether Fold bifurcation or Transcritical/Pirchfork bifurcation occurs.

--- Classes/Stability_SC/@ST_EQ/AdditionalConstraints.m
+++ Classes/Stability_SC/@ST_EQ/AdditionalConstraints.m
@@ -1,6 +1,28 @@
-function idx = AdditionalConstraints(obj,idx,J)
+%This function provides additional constraints to be checked if a
+%bifurcation of an equilibrium solution is detected. This is to check
+%whether a fold bifurcation occurs or a transcritcal/pitchfork
+%bifurcation occurs instead
+%
+%@obj:          object of Stability subclass EQ_ST_PS
+%@idx0:         Index of bifurcation (1 = FB, 2 = HB)
+%@J:            Jacobian at bifurcation point
+%
+%@idx:          Index for occurring bifurcation (1 = FB, 2 = HB, 3 = TCB/PFB)
 
+function idx = AdditionalConstraints(obj,idx0,J)
 
-idx = idx;
+dim = size(J,1)-1;
+if(idx0==1)
+    s = svd(J(1:end-1,:));              % Do an SVD of the extended Jacobian [df/ds,df/dmu]
+    rkJext = sum(abs(sign(s)),1);       % Determine the rank of the extended Jacobian
+
+    if(rkJext<dim)
+        idx = 3;                        % If rank < dim change index to 3 ('TCB/PFB')
+    else
+        idx = idx0;                     % If rank == dim leave index at 1 ('FB')
+    end
+else
+    idx = idx0;                         % If index is 2 ('HB') leave index as is
+end
 
 end
