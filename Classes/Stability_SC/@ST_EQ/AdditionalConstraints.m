98efefc0759d09974a0acbcb0209547183415a8a
Alexander Seifert <alexander.seifert@uni-kassel.de>
2025-07-29 16:30:29 +0200

    Method AdditionalConstraints @ST_EQ now checks whether singular values of extended Jacobian are numerically zero, and sets them equal to zero.

--- Classes/Stability_SC/@ST_EQ/AdditionalConstraints.m
+++ Classes/Stability_SC/@ST_EQ/AdditionalConstraints.m
@@ -11,18 +11,22 @@
 
 function idx = AdditionalConstraints(obj,idx0,J)
 
-dim = size(J,1)-1;
+n = size(J,1);
+dim = n-1;
 if(idx0==1)
-    s = svd(J(1:end-1,:));              % Do an SVD of the extended Jacobian [df/ds,df/dmu]
-    rkJext = sum(abs(sign(s)),1);       % Determine the rank of the extended Jacobian
+    s = svd(J(1:end-1,:));                  % Do an SVD of the extended Jacobian [df/ds,df/dmu]
+    tol = n*max(abs(s),[],'all')*eps;    % Define tolerance below which value is assumed equal to zero
+    s(abs(s) < tol) = 0;                    % Set all values less than tol equal to zero
+
+    rkJext = sum(abs(sign(s)),1);           % Determine the rank of the extended Jacobian
 
     if(rkJext<dim)
-        idx = 3;                        % If rank < dim change index to 3 ('TCB/PFB')
+        idx = 3;                            % If rank < dim change index to 3 ('TCB/PFB')
     else
-        idx = idx0;                     % If rank == dim leave index at 1 ('FB')
+        idx = idx0;                         % If rank == dim leave index at 1 ('FB')
     end
 else
-    idx = idx0;                         % If index is 2 ('HB') leave index as is
+    idx = idx0;                             % If index is 2 ('HB') leave index as is
 end
 
 end
